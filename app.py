ba13fbd7f4cac9932e84408fbf80bb3c67e77724
Roujan Mavarani <Mavarani@hotmail.co.uk>
2022-07-06 23:58:46 +0100

    commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+mail = Mail()
+
+app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'roujanmavarani@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Genesys06!'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail.init_app(app)
+
+
+
+@app.route("/")
+def index():
+  msg = Message('Hello from the other side!',
+                sender =   'roujanmavarani@gmail.com',
+                recipients = ['mavarani@hotmail.co.uk'])
+  msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
+  mail.send(msg)
+  return "Message sent!"
+  
+    
